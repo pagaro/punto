e502a484a55d254ada08632fb7d784aa2e2b432d
pagaro <dev.pagaro@gmail.com>
2023-04-03 18:53:54 +0200

    remove grid

--- app/src/game/gamePage.js
+++ app/src/game/gamePage.js
@@ -1,6 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import axios from "axios";
-import {useNavigate, useParams} from "react-router-dom";
 import Game from "../component/game/Game";
 import NumberOfPlayers from "../component/game/NumberOfPlayers";
 
