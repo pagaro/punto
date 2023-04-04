112eaba4bfe85de64485d7b9981d9eef86e13ae2
pagaro <dev.pagaro@gmail.com>
2023-04-04 12:18:07 +0200

    set comment

--- app/src/component/game/NumberOfPlayers.js
+++ app/src/component/game/NumberOfPlayers.js
@@ -10,11 +10,8 @@ const NumberOfPlayers = ({ onSelect }) => {
     return (
         <div className="number-of-players">
             <h2>Choisissez le nombre de joueurs :</h2>
-            // bouton qui sélectionne 2 joueurs et appelle la fonction handleSelection avec l'argument 2
             <button onClick={() => handleSelection(2)}>2 joueurs</button>
-            // bouton qui sélectionne 3 joueurs et appelle la fonction handleSelection avec l'argument 3
             <button onClick={() => handleSelection(3)}>3 joueurs</button>
-            // bouton qui sélectionne 4 joueurs et appelle la fonction handleSelection avec l'argument 4
             <button onClick={() => handleSelection(4)}>4 joueurs</button>
         </div>
     );
