615af77129bdf66ce89d2f78393b07df189df324
pagaro <dev.pagaro@gmail.com>
2023-04-11 08:07:31 +0200

    docker compose

--- mongo-init.js
+++ mongo-init.js
@@ -7,3 +7,35 @@ db.createUser(
     roles: [{ role: 'readWrite', db: 'punto' }]
   }
 );
+
+db.createCollection('users')
+
+db.users.insertMany([
+  {
+    email: 'toto@gmail.com',
+    dob: ISODate('2023-04-04T00:00:00.000Z'),
+    password: '$2b$10$3GDB9jpLM/EXUBIJE.ER.e4E93LuE6aYKWcF8SydLxHXe7AFLhBy2',
+    name: 'toto',
+  },
+  {
+  email: 'titi@gmail.com',
+  dob: ISODate('2023-04-04T00:00:00.000Z'),
+  password: '$2b$10$3GDB9jpLM/EXUBIJE.ER.e4E93LuE6aYKWcF8SydLxHXe7AFLhBy2',
+  name: 'titi',
+}]);
+
+db.createCollection('games')
+
+db.games.insertMany([
+  {
+    name: 'toto',
+    id_users: [],
+    status: 'WAIT',
+    id_handles: [],
+  },
+  {
+  name: 'titi',
+  id_users: [],
+  status: 'WAIT',
+  id_handles: [],
+}]);
